9d5f45c3d96808f3754d440c917ae28507b4846e
unknown <boureimatapily1@gmail.com>
2020-07-07 18:31:49 +0000

    add redux actions

---
+++ src/redux/Actions/authActions.js
@@ -0,0 +1,72 @@
+
+
+export const register = (email,password, onSuccess)=>{
+
+  return (dispatch, getState,{getFirebase})=>{
+      let firebase = getFirebase()
+      firebase
+          .auth()
+          .createUserWithEmailAndPassword(email,password)
+          .then((user)=>{
+              console.log(user)
+              onSuccess()
+          })
+          .catch((err)=>{
+              console.log(err)
+          })
+  }
+}
+
+
+export const login = (email,password)=>{
+
+  return (dispatch, getState,{getFirebase})=>{
+      let firebase = getFirebase()
+      firebase
+          .auth()
+          .signInWithEmailAndPassword(email,password)
+          .then((user)=>{
+              console.log(user)
+              
+          })
+          .catch((err)=>{
+              console.log(err)
+          })
+  }
+}
+
+export const logout = ()=>{
+  console.log("logout")
+  return (dispatch, getState,{getFirebase})=>{
+      let firebase = getFirebase()
+      firebase
+          .auth()
+          .signOut()
+          .then((user)=>{
+              console.log(user)
+          })
+          .catch((err)=>{
+              console.log(err)
+          })
+  }
+}
+
+export const authenticateWithGoogle = (onSuccess)=>{
+  return (dispatch, getState,{getFirebase})=>{
+      let firebase = getFirebase()
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase
+          .auth().signInWithPopup(provider)
+          .then((user)=>{
+              console.log(user)
+              onSuccess()
+          })
+          .catch((err)=>{
+              console.log(err)
+          })
+  }
+}
+
+
+
+
